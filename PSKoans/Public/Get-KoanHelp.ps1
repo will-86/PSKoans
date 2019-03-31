8add693d90f3269fd17f7d38cc7697b7c20ca7d0
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-03-31 02:54:43 -0400

    :sparkles: Add Get-KoanHelp

---
+++ PSKoans/Public/Get-KoanHelp.ps1
@@ -0,0 +1,19 @@
+﻿function Get-KoanHelp {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+        [string[]]
+        $Topic
+    )
+    process {
+        if ($MyInvocation.ExpectingInput) {
+            $Topic = , $Topic
+        }
+
+        foreach ($Item in $Topic) {
+            Get-ChildItem -Path $script:ModuleRoot -Filter "${Item}*" -Recurse |
+            Select-Object -First 1 |
+            Get-Help -Online -Name { $_.FullName }
+        }
+    }
+}
