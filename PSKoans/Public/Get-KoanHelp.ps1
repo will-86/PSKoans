221d0d4f54924d1d2e34541bf5623d5413dcc804
vexx32 <32407840+vexx32@users.noreply.github.com>
2019-03-31 02:59:42 -0400

    :memo: add CBH and argumentcompleter

--- PSKoans/Public/Get-KoanHelp.ps1
+++ PSKoans/Public/Get-KoanHelp.ps1
@@ -1,7 +1,46 @@
 ﻿function Get-KoanHelp {
+    <#
+    .SYNOPSIS
+    Get-KoanHelp provides access to more extensive documentation on the koan topics.
+
+    .DESCRIPTION
+    Get-KoanHelp calls Get-Help -Online on the koan topic(s) that you specify to provide further assistance and more
+    extensive documentation, should you need it or be interested in it.
+
+    .PARAMETER Topic
+    Specify one or more topics to query the help pages for.
+
+    .EXAMPLE
+    Get-KoanHelp -Topic AboutArrays
+
+    Opens the about_Arrays help documentation for you to read through in your default browser.
+
+    .EXAMPLE
+    Get-KoanHelp AboutArrays
+
+    Opens the about_Arrays help documentation in your default browser.
+
+    .NOTES
+    Only koan files with a preset HelpUri in their [CmdletBinding()] attribute will be functional with this command.
+    #>
+
     [CmdletBinding()]
     param(
         [Parameter(Position = 0, Mandatory, ValueFromPipeline)]
+        [Alias('Koan', 'File')]
+        [ArgumentCompleter(
+            {
+                param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams)
+
+                $Values = Get-PSKoanLocation | Get-ChildItem -Recurse -Filter '*.Koans.ps1' |
+                Sort-Object -Property BaseName |
+                ForEach-Object {
+                    $_.BaseName -replace '\.Koans$'
+                }
+
+                return @($Values) -like "$WordToComplete*"
+            }
+        )]
         [string[]]
         $Topic
     )
